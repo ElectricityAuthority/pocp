86e5702775532e4d7259b405bd76041100e74ad5
Joel Miller <jol123@gmail.com>
2014-03-05 14:26:38 +1300

    Added shell script to run python script and commit/push data updates to github

---
+++ pocp.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# This shell script runs the pocp.py python script to download and process the
+# pocp outage data.
+#
+# The processed data is then pushed to github as walter-ea. This requires the github
+# remote to be set up as ssh rather than https
+PYTHONPATH=/home/dave/python:/home/dave/python/EAtools
+
+# Go to the pocp directory and ensure that the master branch is checked out and has
+# latest version.
+cd /home/dave/python/pocp
+git checkout master
+git pull
+
+# Run the python script
+/usr/bin/python pocp.py >> pocp.log 2>&1
+
+# Commit the changes and push to github
+DATE=$(date +'%Y-%m-%d %H:%M:%S')
+git commit -a --author="walter ea <walter.ea4@gmail.com>" -m "Data Update $DATE"
+git push
+
+# Merge the changes into the gh-pages branch
+git checkout gh-pages
+git pull
+git merge master
+git push
+git checkout master
