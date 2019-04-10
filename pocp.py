a333499eceb1be34abb8b3488bbee4ebeaca4d16
walter ea <walter.ea4@gmail.com>
2019-04-11 11:13:51 +1200

    Data Update 2019-04-11 11:13:50
    Former-commit-id: 48c61580ef6a66638fc3f610641f3c73136a4b4c

--- pocp.py
+++ pocp.py
@@ -121,6 +121,8 @@ class POCP(object):
         logger.info("Login to POCP site")
         self.br = mechanize.Browser(factory=mechanize.RobustFactory())
         self.br.set_handle_robots(False)
+        self.br.addheaders = [('User-agent',
+                               'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
         info_text = "Attempting to open " + self.cmd_line.pocp_host
         logger.info(info_text)
         self.br.open(self.cmd_line.pocp_host)
